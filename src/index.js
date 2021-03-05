53aa0d8d4010f80408e1c27efcb434827a362b79
Tânagra Andria <design.andria@gmail.com>
2021-03-05 17:45:07 -0300

    Private Route

--- src/index.js
+++ src/index.js
@@ -6,16 +6,31 @@ import Waiter from './pages/Waiter';
 import Kitchen from './pages/Kitchen';
 import Historic from './pages/Historic';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { isAuthenticated } from "./route-auth";
+
+const PrivateRoute = ({component: Component, ...rest})=>(
+  <Route 
+    {...rest} 
+    render ={props =>(
+      isAuthenticated()? (
+        <Component{...props}/>
+      ) : (
+        <Redirect to ={{pathname: '/login', state:{ from: props.location}}} />
+      )
+      )      
+    }
+  />
+);
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path='/' component={Login} exact />
       <Route path='/SignUp' component={SignUp} exact /> 
-      <Route path='/waiter' component={Waiter} exact /> 
-      <Route path='/kitchen' component={Kitchen} exact /> 
-      <Route path='/Historic' component={Historic} /> 
+       <PrivateRoute path ='/waiter' component={Waiter} exact /> 
+       <PrivateRoute path ='/kitchen' component={Kitchen} exact /> 
+       <PrivateRoute path ='/Historic' component={Historic} /> 
       <Route component={() => <div>Page 404</div>}/>
     </Switch>
   </BrowserRouter>,
